93710340cecd63d7617441892bd2aeef70f1dc0d
ritika86 <34647804+ritika86@users.noreply.github.com>
2018-02-13 18:50:08 +0530

    Update setup-vars.sh

--- scripts/setup-vars.sh
+++ scripts/setup-vars.sh
@@ -5,11 +5,11 @@ if [ -e ~/.setup/data.sh ]; then
 else
     echo -n "Scheme (default http): "
     read scheme
-    echo -n "Hostname (default: localhost:8000): "
+    echo -n "Hostname (default: 18.219.166.205:8000): "
     read hostname
 
     if [ -z "$hostname" ]; then
-        hostname="localhost:8000"
+        hostname="18.219.166.205:8000"
     fi
 
     if [ -z "$scheme" ]; then
